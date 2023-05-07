a968c55518675b053ade5cfdc0ced5de74b9f409
Ian Scott Martin <ianscottmartin@gmail.com>
2023-05-07 15:52:33 -0600

    corb errors

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-function renderOneArtist(artist){
+function renderOneArtist(artwork){
     //Build Artist
-    let card = document.createElement('li')
+    let card = document.createElement('ul')
     card.className = 'card'
     card.innerHTML = `
-   <img src="${artist.imageUrl}" 
+   <img src="${artwork.primaryImage}" 
     <div class="content">
-    <h4>${artist.name}</h4>
+    <h4>${artwork.name}</h4>
     <p>
-        $<span class="count">${artist.donations}</span> Donated
+        $<span class="count">${artwork.donations}</span> Donated
     </p>
-    <p>${artist.description}</p>
+    <p>${artwork.description}</p>
     </div>
     <div class="buttons">
         <button> Donate $10 </button>
@@ -24,7 +24,7 @@ function renderOneArtist(artist){
  function getAllArtists(){
     fetch('http://localhost:3000/artworks') 
         .then(res => res.json())
-        .then(artworks => artworks.forEach(artworks => renderOneArtist(artworks)))
+        .then(artworks => artworks.forEach(artwork => renderOneArtist(artwork)))
             
         }
  
@@ -36,87 +36,3 @@ function initialize(){
     console.log("get all artists")
 }
 initialize()
-
-
-
-
-// {
-//         document.addEventListener("DOMContentLoaded", function () {
-//             console.log('The DOM has loaded');
-//         });
-
-//         console.log("this loads the dom");
-    
-
-//         console.log("works")
-
-//         fetch("http:localhost:3000/artworks")
-//             .then(response => response.json())
-//             .then(artworks => {
-//                 const container = document.querySelector("#artworkList");
-//                 //   artworks.forEach(artwork => {
-//                 //   })
-
-//                 //   });
-//             })
-
-//         fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects")
-//             .then(function (response) {
-//                 return response.json();
-//             })
-//             .then(function (title) {
-//                 console.log(title);
-
-//             });
-//     };
-
-
-
-
-//     // console.log("works")
-//     // fetch("http:localhost:3000/artworks")
-//     //     .then(response => response.json())
-//     //     .then(artworks => {
-//     //         const container = document.querySelector("ul#artworkList");
-//     //         //   artworks.forEach(artwork => {
-//     //         //   })
-
-//     //         //   });
-//     //     })
-
-//     // fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects")
-//     //     .then(function (response) {
-//     //         return response.json();
-//     //     })
-//     //     .then(function (title) {
-//     //         console.log(title);
-
-//     //     });
-//     fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID,isHighlight, primarySmallImage, artistDisplayName, artistsDisplayBio]")
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (objectID) {
-//             console.log(objectID);
-//         });
-//     // fetch("https://collectionapi.metmuseum.org/public/collection/v1/search")
-//     //     .then(function (res) {
-//     //         return res.json();
-
-//     //     })
-//     //     .then(function (objectsIDs) {
-//     //         console.log(objectsIDs);
-//     //     });
-
-//     fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?isOnView=true&q=van gogh")
-//         .then(function (res) {
-//             return res.json();
-//         })
-//         .then(function (isOnView) {
-//             console.log(isOnView)
-//         });
-//         fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]")
-    
-
-
-
